2d08bdf6c5f377edccec057e82ff83ed6c3c6bab
Megan Palacios <meg2086@outlook.com>
2022-05-25 10:51:34 -0500

    refactored absolutify, created pathExists, isFile and isDirectory with sync, and isMd

--- src/md-links.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import fs from 'fs';
-
-export function absolutify(route) {
-    let absolute = '';
-    path.isAbsolute(route) ? absolute = route : absolute = path.resolve(route);
-    return absolute;
-}
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+import path from 'path';
+import fs from 'fs';
+
+export const absolutify = (route) => path.isAbsolute(route) ? route : path.resolve(route);
+
+export const pathExists = (route) => fs.existsSync(route);
+
+export const isFile = (route) => fs.statSync(route).isFile();
+
+export const isDirectory = (route) => fs.statSync(route).isDirectory();
+
+export const isMd = (route) => path.extname(route) === '.md';
