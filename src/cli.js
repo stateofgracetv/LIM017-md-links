24eb67aa869772097da5484fe2db5f7a71d1c8ff
Megan Palacios <meg2086@outlook.com>
2022-06-06 22:58:29 -0500

    First attempt at adding flags into flow

--- src/cli.js
+++ src/cli.js
@@ -5,11 +5,23 @@
 }; */
 
 import { mdLinks } from "./index.js";
-const pathToFile = process.argv[2] ;
+import process from 'process';
+const pathToFile = process.argv[2];
+
+const findFlags = (key) => {
+  if (process.argv.includes(`--${ key }`)) return true;
+  else return false;
+}
+
+let validate = findFlags('validate');
+console.log('validate:', validate);
+let stats = findFlags('stats');
+console.log('stats', stats);
 
 try {
-  mdLinks(pathToFile)
+  mdLinks(pathToFile, validate, stats)
     .then(extractedLinks => console.log(extractedLinks))
+    // here parse the results from extractedLinks with a forEach
     .catch(error => console.log(`Error detected! ${error}`));
 } catch (error) {
   console.log(`Caught by try/catch ${error}`);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { absolutify, pathExists, isFile, isMd, isDirectory, scanDir, extractLinks, extractedLinks } from './utils.js'
+import { absolutify, pathExists, isFile, isMd, isDirectory, scanDir, extractLinks, extractedLinks, validateMyLinks } from './utils.js'
 import chalk from 'chalk';
 
-export const mdLinks = (route, options) => new Promise ((resolve, reject) => {
+export const mdLinks = (route, validate, stats) => new Promise ((resolve, reject) => {
     const absoluteRoute = absolutify(route);
     if (!pathExists(absoluteRoute)) {
         throw new Error("Invalid: Path does not exist");
@@ -17,5 +17,9 @@ export const mdLinks = (route, options) => new Promise ((resolve, reject) => {
 
     if (extractedLinks.length < 1) {
         throw new Error('No links were found');
+    }
+
+    if (validate) {
+        validateMyLinks(extractedLinks);
     }
 });
