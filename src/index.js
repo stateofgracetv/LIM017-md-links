e44a99b79117437520adf8c1129b21dd68104ed4
Megan Palacios <meg2086@outlook.com>
2022-06-09 11:14:23 -0500

    modified index.js with a switch for validate true and false, prints the array of objects raw from cli

--- src/index.js
+++ src/index.js
@@ -6,22 +6,41 @@ export const mdLinks = (route, validate) => new Promise ((resolve, reject) => {
         throw new Error("Invalid: Path does not exist");
     }
     
-    if (isDirectory(absoluteRoute)) {
-        resolve(scanDir(absoluteRoute));
-    } else if (isFile(absoluteRoute) && isMd(absoluteRoute)) {
-        resolve(extractLinks(absoluteRoute));
-    } else if (isFile(absoluteRoute) && !isMd(absoluteRoute)) {
-        throw new Error('Path is not an .md file');
+    switch (validate) {
+        case false:
+            if (isDirectory(absoluteRoute)) {
+                resolve(scanDir(absoluteRoute));
+            } else if (isFile(absoluteRoute) && isMd(absoluteRoute)) {
+                resolve(extractLinks(absoluteRoute));
+            } else if (isFile(absoluteRoute) && !isMd(absoluteRoute)) {
+                throw new Error('Path is not an .md file');
+            }
+            break;
+        case true:
+            if (isDirectory(absoluteRoute)) {
+                scanDir(absoluteRoute);
+                const validatedLinks = validateMyLinks(extractedLinks)
+                .then(validatedLinks => resolve(validatedLinks))
+                .catch(err => console.log(err))
+            } else if (isFile(absoluteRoute) && isMd(absoluteRoute)) {
+                extractLinks(absoluteRoute);
+                const validatedLinks = validateMyLinks(extractedLinks)
+                .then(validatedLinks => resolve(validatedLinks))
+                .catch(err => console.log(err))
+            } else if (isFile(absoluteRoute) && !isMd(absoluteRoute)) {
+                throw new Error('Path is not an .md file');
+            }
     }
+    
 
     if (extractedLinks.length < 1) {
         throw new Error('No links were found');
     }
 
-    if (validate) {
+    /* if (validate) {
         const validatedLinks = validateMyLinks(extractedLinks)
         // resolve
         .then(validatedLinks => console.log(validatedLinks))
         .catch(err => console.log(err))
-    }
+    } */
 });
