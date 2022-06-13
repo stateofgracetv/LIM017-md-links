fe5d4a5c2fcdec55c2e06793c93e1cfac4b38855
Megan Palacios <meg2086@outlook.com>
2022-06-13 11:35:12 -0500

    Close #28, fully functioning stats as per the README

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { mdLinks } from "./index.js";
+import { mdLinks, reportStats } from "./index.js";
 import chalk from 'chalk';
 import process from 'process';
 const commands = process.argv;
@@ -15,30 +15,47 @@ const findFlags = (key) => {
 let validate = findFlags('validate');
 let stats = findFlags('stats');
 
-// console.log(commands.length);
-// execute
-/* switch (commands.lenght) {
-  case 2:
-    console.log('Welcome to this md-links library, enter --help to know how it works')
-} */
+function execute() {
+  switch (stats) {
+    case false:
+      try {
+        const result = mdLinks(pathToFile, validate)
+        .then(result => {
+          const numKeys = Object.keys(result[0]).length;
+          switch (numKeys) {
+            case 3: // no validate
+              result.forEach(el => {
+                console.log(chalk.bgGreen(pathToFile), chalk.underline(el.href), chalk.bold(el.text.slice(0, 50)));
+              });
+              break;
+            case 5: // with validate
+              result.forEach(el => {
+                console.log(chalk.bgGreen(pathToFile), chalk.underline(el.href), chalk.blue.bold(el.ok), chalk.bold.green(el.status), chalk.bold(el.text.slice(0, 50)));
+              });
+          }
+        })
+        .catch(error => console.log(chalk.red.bold(`Error detected! ${error}`)));
+      } catch (error) {
+        console.log(`Caught by try/catch ${error}`);
+      }
+      break;
 
-try {
-  const result = mdLinks(pathToFile, validate)
-  .then(result => {
-    const numKeys = Object.keys(result[0]).length;
-    switch (numKeys) {
-      case 3: // no validate
-        result.forEach(el => {
-          console.log(chalk.bgGreen(pathToFile), chalk.underline(el.href), chalk.bold(el.text.slice(0, 50)));
-        });
-        break;
-      case 5: // with validate
-        result.forEach(el => {
-          console.log(chalk.bgGreen(pathToFile), chalk.underline(el.href), chalk.blue.bold(el.ok), chalk.bold.green(el.status), chalk.bold(el.text.slice(0, 50)));
-        });
-    }
-  })
-  .catch(error => console.log(chalk.red.bold(`Error detected! ${error}`)));
-} catch (error) {
-  console.log(`Caught by try/catch ${error}`);
+    case true:
+      try {
+        const resultStats = reportStats(pathToFile, validate)
+        .then(resultStats => {
+          console.log(chalk.white('Total:', resultStats.total));
+          console.log(chalk.blue('Unique:', resultStats.unique));
+          if (resultStats.broken) {
+            console.log(chalk.red('Broken:', resultStats.broken));
+          }
+        })
+        .catch(error => console.log(chalk.red.bold(`Error detected! ${error}`)));
+      } catch (error) {
+        console.log(`Caught by try/catch ${error}`);
+      }
+      break;
+  }
 }
+
+execute();
